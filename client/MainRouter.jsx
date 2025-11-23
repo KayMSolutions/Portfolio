1da497695dee8d49d125c70e50fd3c480a481bd7
Kaylie Moskal <kayliemoskal@gmail.com>
2025-11-22 19:02:06 -0500

    Add files via upload

--- client/MainRouter.jsx
+++ client/MainRouter.jsx
@@ -7,21 +7,73 @@ import Footer from "./src/components/Footer.jsx";
 
 import About from "./src/components/About.jsx";
 import Education from "./src/components/Education.jsx";
-import Project from "./src/components/Project.jsx";   // filename is Project.jsx
+import Project from "./src/components/Project.jsx";
 import Contact from "./src/components/Contact.jsx";
 import Services from "./src/components/Services.jsx";
+
+import Signin from "./src/auth/Signin.jsx";
+import Signup from "./src/auth/Signup.jsx";
+
+import AdminContacts from "./src/admin/AdminContacts.jsx";
+import AdminProjects from "./src/admin/AdminProjects.jsx";
+import AdminQualifications from "./src/admin/AdminQualifications.jsx";
+import AdminDashboard from "./src/admin/AdminDashboard.jsx";
+
+import PrivateRoute from "./src/auth/PrivateRoute.jsx";
 
 export default function MainRouter() {
   return (
     <BrowserRouter>
       <Layout />
       <Routes>
+        {/* Public Pages */}
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/education" element={<Education />} />
         <Route path="/projects" element={<Project />} />
         <Route path="/services" element={<Services />} />
         <Route path="/contact" element={<Contact />} />
+
+        {/* Auth */}
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/signup" element={<Signup />} />
+
+        {/* Admin Pages (Protected) */}
+        <Route
+          path="/admin/contacts"
+          element={
+            <PrivateRoute>
+              <AdminContacts />
+            </PrivateRoute>
+          }
+        />
+
+        <Route
+          path="/admin/projects"
+          element={
+            <PrivateRoute>
+              <AdminProjects />
+            </PrivateRoute>
+          }
+        />
+
+        <Route
+          path="/admin/qualifications"
+          element={
+            <PrivateRoute>
+              <AdminQualifications />
+            </PrivateRoute>
+          }
+        />
+
+        <Route
+          path="/admin/dashboard"
+          element={
+            <PrivateRoute>
+              <AdminDashboard />
+            </PrivateRoute>
+          }
+        />
       </Routes>
       <Footer />
     </BrowserRouter>
